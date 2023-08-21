cb352f7fd571a3d5c8daf6e6f7befa9da96d4351
Nkaleth <nseguralu@gmail.com>
2023-08-21 11:15:54 -0500

    ADD DB config to access

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-//App config
+// App config
 
 const app = express();
 const port = process.env.port || 8001;
+const connectionUrl = 'mongodb+srv://nsegura:Kaleth123@cluster0.y7ntg95.mongodb.net/?retryWrites=true&w=majority';
 
 // Middleware
 
 // DB Config
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopoly: true,
+});
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello TheWebDev'));
